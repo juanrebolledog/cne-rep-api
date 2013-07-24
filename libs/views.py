bf4a00f8cf81f8509c57657b5f711675678a1b30
Juan Francisco Rebolledo Gómez <juanrebolledog@gmail.com>
2013-07-24 14:04:39 -0430

    New route to query voting centers, accepts state, municipality and parish url args

--- libs/views.py
+++ libs/views.py
@@ -1,9 +1,10 @@
 from libs import app
-from flask import Response
+from flask import Response, request
 from libs.models import Voter, Center
 import json
 import logging
 from werkzeug.contrib.cache import RedisCache
+import hashlib
 
 
 cache = RedisCache()
@@ -29,3 +30,22 @@ def search_voters_center(centerid):
 def search_center(centerid):
     center = Center.query.filter_by(code=centerid).first()
     return Response(json.dumps(center.todict()), mimetype='application/json')
+
+@app.route('/centers')
+def search_centers_filter():
+    args = {
+        'state_code': request.args.get('state', ''),
+        'municipality_code': request.args.get('municipality', ''),
+        'parish_code': request.args.get('parish', '')
+    }
+    filter_hash = hashlib.sha1(args['state_code'] + args['municipality_code'] + args['parish_code']).hexdigest()
+    for i in args.keys():
+        if args[i] == '':
+            del args[i]
+
+    centers = cache.get('centers-filter-' + str(filter_hash))
+    if centers is None:
+        centers = Center.query.filter_by(**args).all()
+        centers = map(Center.todict, centers)
+        cache.set('centers-filter-' + str(filter_hash), centers, timeout=5 * 60)
+    return Response(json.dumps(centers), mimetype='application/json')
